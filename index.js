73522dde0b291af7df5d1b7e1b1682f6cb681859
Aanshi39 <aanshigupta109@gmail.com>
2024-07-20 12:31:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*{}[()]/><?";
+
+function createPassword(){
+    let password = "";
+    password += upperCase{Math.floor(Math.random () * upperCase.length)} ;
+
+}
